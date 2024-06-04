2df03b2c121cc1ba39cfcc99e9b168e897231840
sarah tuccitto <sarah.tuccitto@hotmail.fr>
2024-06-04 19:46:54 +0200

    modification dans le html et ajout d'un fichier js

---
+++ sarah.js
@@ -0,0 +1,39 @@
+const playAudioLinkDenis = document.querySelector("#audioLinkDenis");
+const audioDenis = document.querySelector("#audioDenis");
+
+playAudioLinkDenis.addEventListener("click", (event) => {
+    event.preventDefault();
+    audioDenis.play();
+});
+
+const playAudioLinkSouffrir = document.querySelector("#audioLinkSouffrir");
+const audioSouffrir = document.querySelector("#audioSouffrir")
+
+playAudioLinkSouffrir.addEventListener("click", (event) => {
+    event.preventDefault();
+    audioSouffrir.play();
+});
+
+const playAudioLinkKamoulox = document.querySelector("#audioLinkKamoulox");
+const audioKamoulox = document.querySelector("#audioKamoulox")
+
+playAudioLinkKamoulox.addEventListener("click", (event) => {
+    event.preventDefault();
+    audioKamoulox.play();
+});
+
+const playAudioLinkRisitas = document.querySelector("#audioLinkRisitas");
+const audioRisitas = document.querySelector("#audioRisitas")
+
+playAudioLinkRisitas.addEventListener("click", (event) => {
+    event.preventDefault();
+    audioRisitas.play();
+});
+
+const playAudioLinkVelo = document.querySelector("#audioLinkVelo");
+const audioVelo = document.querySelector("#audioVelo")
+
+playAudioLinkVelo.addEventListener("click", (event) => {
+    event.preventDefault();
+    audioVelo.play();
+});
